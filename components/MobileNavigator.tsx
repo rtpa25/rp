eaed4d65818d20ae696491bc8987b5b4d0bfb1a5
Ronit Panda <pandaronit25@gmail.com>
2022-01-03 09:31:18 +0530

    changed the resume added it to the mob nav bar

--- components/MobileNavigator.tsx
+++ components/MobileNavigator.tsx
@@ -34,7 +34,7 @@ const MobileNavigator: React.FC<ModalProps> = ({ show, onClose }) => {
 
   const modalContent = show ? (
     <div
-      className='absolute top-0 left-0 w-full h-full duration-300 ease-in-out bg-gray-800/60 cursor-pointer'
+      className='absolute top-0 left-0 w-full h-full duration-300 ease-in-out cursor-pointer bg-gray-800/60'
       onClick={handleClose}>
       <aside className='absolute top-0 right-0 z-50 w-9/12 h-full duration-300 ease-in-out bg-gradient-to-r from-blue-500 to-blue-800'>
         <div>
@@ -68,11 +68,13 @@ const MobileNavigator: React.FC<ModalProps> = ({ show, onClose }) => {
                 Contact
               </li>
             </ul>
-            <button className='my-10 relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-800'>
+            <a
+              className='my-10 relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-800'
+              href='/resume.pdf'>
               <span className='text-2xl relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0'>
                 Resume
               </span>
-            </button>
+            </a>
           </div>
         </div>
       </aside>
